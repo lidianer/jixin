0a2f8a18a47d48b28ac70f940c1d5d9f3952adcb
lidianer <lidianer@126.com>
2022-09-25 10:14:50 +0800

    0925

---
+++ src/main/java/com/unicom/jixin/JixinApplication.java
@@ -0,0 +1,13 @@
+package com.unicom.jixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JixinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JixinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicom/jixin/JixinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicom.jixin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JixinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
